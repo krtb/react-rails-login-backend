1a1c628fa14424bc587b01c539c60cf19a317545
Kurt Bauer <kurtbauer55@protonmail.com>
2019-12-17 18:19:06 -0500

    correct typo

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   
-  # imp for Front-End user registration of new Users 
+  # imp for Front-End registration of new Users 
   get '/logged_in', to: 'sessions#is_logged_in?'
       
   resources :users, only: [:create, :show, :index]
