e71d84ce85ca38992a9c8806ecaa931d2c245404
Kurt Bauer <kurtbauer55@protonmail.com>
2019-12-17 17:13:56 -0500

    add show action method to UserController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UserController < ApplicationController
 
     def index
         @users = User.all
+
         if @users
         render json: {
             users: @users
@@ -10,6 +11,22 @@ class UserController < ApplicationController
         render json: {
             status: 500,
             errors: ['no users found']
+        }
+        end
+
+    end
+
+    def show
+        @user = User.find(params[:id])
+        
+        if @user
+        render json: {
+            user: @user
+        }
+        else
+        render json: {
+            status: 500,
+            errors: ['user not found']
         }
         end
     end
