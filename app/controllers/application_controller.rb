b2317732ea65c32a1c8d5af3519346bb5d8ee869
Kurt Bauer <kurtbauer55@protonmail.com>
2019-12-17 17:36:01 -0500

    skip verify_auth_token from rails, disable errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+    # disable this so that we don’t receive ‘forbidden’ parameters
+    # that will prevent our controller actions from executing without errors.
+    # security token that Rails generates from our session data and adds to the parameters 
+    # sent from a Rails form to a controller action to prevent cross-site request forgery (CSRF) attacks
+    skip_before_action :verify_authenticity_token
 end
