bb8dc1bbec68759cbf4901e7d5cc812e1eb5324c
Kurt Bauer <kurtbauer55@protonmail.com>
2019-12-17 17:13:15 -0500

    add index action method to UserController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,17 @@
 class UserController < ApplicationController
 
+    def index
+        @users = User.all
+        if @users
+        render json: {
+            users: @users
+        }
+        else
+        render json: {
+            status: 500,
+            errors: ['no users found']
+        }
+        end
+    end
+
 end
