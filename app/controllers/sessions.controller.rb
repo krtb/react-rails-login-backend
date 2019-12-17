552efee07bd5596732e0282d131f93adac6b19e4
Kurt Bauer <kurtbauer55@protonmail.com>
2019-12-17 18:00:30 -0500

    add create action method for SessionController

--- app/controllers/sessions.controller.rb
+++ app/controllers/sessions.controller.rb
@@ -1,3 +1,24 @@
 class SessionsController < ApplicationController
 
+    def create
+        @user = User.find_by(email: session_params[:email])
+    
+        if @user && @user.authenticate(session_params[:password])
+        login! # session[:user_id] = @user.id, from application_controller.rb
+
+        render json: {
+            logged_in: true,
+            user: @user
+        }
+
+        else
+
+        render json: { 
+            status: 401,
+            errors: ['no such user', 'verify credentials and try again or signup']
+        }
+        
+        end
+    end
+
 end
