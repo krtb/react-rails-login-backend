ce198574aed76e82912dad852519991d54375902
Kurt Bauer <kurtbauer55@protonmail.com>
2019-12-17 17:47:37 -0500

    add method to see if user is stored current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,4 +19,9 @@ class ApplicationController < ActionController::Base
         @current_user ||= User.find(session[:user_id]) if session[:user_id]
     end
 
+    def authorized_user?
+        @user == current_user
+    end
+
+
 end
