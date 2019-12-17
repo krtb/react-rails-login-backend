1f57328929683227893f0897a12b358dcd9c1c5a
Kurt Bauer <kurtbauer55@protonmail.com>
2019-12-17 17:06:16 -0500

    add validations for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ApplicationRecord
+    # bcrypt uses an attribute called has_secure_password We need to include this in our User model in order for bcrypt to work. 
+    has_secure_password
+
+    # VALIDATIONS FOR => USERNAME
+    validates :username, presence: true
+    validates :username, uniqueness: true
+    validates :username, length: { minimum: 4 }
+
+    # VALIDATIONS FOR => EMAIL
+    validates :email, presence: true
+    validates :email, uniqueness: true
+    validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 end
